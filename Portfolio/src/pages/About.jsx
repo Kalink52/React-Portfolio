a265d01630689fdf051377fe9e08404cd98b37ad
Kalink52 <164951917+Kalink52@users.noreply.github.com>
2024-08-29 13:26:05 -0400

    portfoolio page is layed out, removed everything from being a flex box

--- Portfolio/src/pages/About.jsx
+++ Portfolio/src/pages/About.jsx
@@ -4,22 +4,22 @@ function About () {
     return(
 
         <>
-        <container className="grid grid-flow-row grid-cols-2 grid-rows-3">
+        <container className="grid grid-flow-row grid-cols-3 grid-rows-3">
 
-                <img className="h-auto max-w-40" src="src/assets/selfie.jpg" alt="selfie" />
-                <p className="max-w-max min-w max">
+                <img className="h-auto max-w-40 " src="src/assets/selfie.jpg" alt="selfie" />
+                <p className="col-span-2">
                 In the digital cosmos where algorithms dance and virtual worlds thrive, Micah Cox emerges as a titan of the code, a master of the joystick, and a virtuoso of melodies. With a keyboard as their wand, Micah crafts intricate digital realms where lines of code orchestrate symphonies of logic and innovation. They navigate gaming landscapes with the finesse of a seasoned strategist, conquering quests and challenges with unwavering determination.
                 </p>
 
 
-                <p className="">
+                <p className="col-span-2">
                 Beyond the pixels and polygons, Micah finds solace in the harmonies of music, where strings and keys become their playground. From soaring guitar riffs to delicate piano melodies, they wield music as a canvas for expression, creating compositions that resonate deeply with their soul.
                 </p>
                 <img className="" src="" alt="selfie" />
 
 
                 <img className="" src="" alt="selfie" />
-                <p>
+                <p className="col-span-2">
                 In the epic saga of Micah Cox, the worlds of coding, gaming, and music converge, forming a tapestry of creativity and passion that knows no bounds. With each line of code, each gaming triumph, and each musical masterpiece, Micah leaves an indelible mark on the digital landscape, inspiring all who encounter their legendary exploits.
                 </p>
        </container>
