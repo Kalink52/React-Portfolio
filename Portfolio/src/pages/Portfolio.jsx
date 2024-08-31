78c3f41bb65fbb0ac8bd8222cd98fb4c2d3bc5da
Kalink52 <164951917+Kalink52@users.noreply.github.com>
2024-08-30 21:32:26 -0400

    filled out portfolio information

--- Portfolio/src/pages/Portfolio.jsx
+++ Portfolio/src/pages/Portfolio.jsx
@@ -9,7 +9,7 @@ import {
     Button,
   } from "@material-tailwind/react";
 const buttonStyling = "rounded-md bg-dark-gray py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" 
-
+// TODO PERSONAL: Use Github api to display last commit, and last commit message
 const portfolioList = [
     {
         id: 1,
@@ -17,13 +17,13 @@ const portfolioList = [
         description: 'Here is my projects using UE4 and their public status',
         pictureURL: 'src/assets/portfolioPictures/UnrealEngine.PNG',
         deployedUrl: "" ,
-        repoUrl: "",
+        repoUrl: "https://github.com/Kalink52/FableClone",
 
     },
     {
         id: 2,
         title: 'Tech Blog',
-        description: 'TODO',
+        description: 'An attempt at making a tech blog application with authentication',
         pictureURL: 'src/assets/portfolioPictures/Dashboard.PNG',
         deployedUrl: "https://tech-blog-kisx.onrender.com/" ,
         repoUrl: "https://github.com/Kalink52/tech-blog",
@@ -32,26 +32,26 @@ const portfolioList = [
     {
         id: 3,
         title: 'Green Acres',
-        description: 'Here is my projects using UE4 and their public status',
+        description: 'TODO REDO ALL HTML AND CSS and Render from my account NOT WORKING AS DATABASE IS DEPRECATED',
         pictureURL: 'src/assets/portfolioPictures/GreenAcres.PNG',
-        deployedUrl: "https://project2-hivl.onrender.com/" ,
+        deployedUrl: "https://green-acres.onrender.com" ,
         repoUrl: "https://github.com/Kalink52/Green-Acres",
     },
     {
         id: 4,
-        title: 'Unreal Engine',
-        description: 'Here is my projects using UE4 and their public status',
-        pictureURL: 'src/assets/portfolioPictures/UnrealEngine.PNG',
-        deployedUrl: "" ,
-        repoUrl: "",
+        title: 'Tune Tracker',
+        description: 'My first collaboration attempt just to  figure out gitflow, and introducing using Apis,',
+        pictureURL: 'src/assets/portfolioPictures/TuneTracker.PNG',
+        deployedUrl: "https://github.com/Kalink52/tune_tracker/deployments/github-pages" ,
+        repoUrl: "https://github.com/Kalink52/tune_tracker",
     },
     {
         id: 5,
-        title: 'Unreal Engine',
-        description: 'Here is my projects using UE4 and their public status',
-        pictureURL: 'src/assets/portfolioPictures/UnrealEngine.PNG',
-        deployedUrl: "" ,
-        repoUrl: "",
+        title: 'Book Buddies',
+        description: 'TODO: GET TO SHOW ON RENDER. A project a colleague was working on where they needed me to redo their SQL database. I had to work with them to figure out the type of relationships their database needed and assisted in incorporating it into the database using Postgres. ',
+        pictureURL: 'src/assets/portfolioPictures/BookBuddies.PNG',
+        deployedUrl: "https://book-buddies.onrender.com" ,
+        repoUrl: "https://github.com/Kalink52/book-buddies/tree/main",
     },
     {
         id: 6,
