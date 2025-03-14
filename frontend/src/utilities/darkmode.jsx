67c4f5ee652ca9b0c17304ad535e02b8503bbf2b
Kalink Cox <micah.cox52@gmail.com>
2025-03-13 21:18:16 -0400

    readded docker to this project testing nginx

--- frontend/src/utilities/darkmode.jsx
+++ frontend/src/utilities/darkmode.jsx
@@ -1,15 +1,16 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-export default function darkMode(){
-  console.log('reached')
-if (
-  localStorage.theme !== 'dark' 
-  // || (!('theme' in localStorage)
-  //  && window.matchMedia('(prefers-color-scheme: dark)').matches
+export default function darkMode() {
+  console.log(window.matchMedia);
+  console.log("reached");
+  if (
+    localStorage.theme !== "dark"
+    // || (!('theme' in localStorage)
+    //  && window.matchMedia('(prefers-color-scheme: dark)').matches
   ) {
-    localStorage.theme = 'dark' 
-    document.documentElement.classList.add('dark')
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
   } else {
-    localStorage.removeItem('theme')
-    document.documentElement.classList.remove('dark')
+    localStorage.removeItem("theme");
+    document.documentElement.classList.remove("dark");
   }
 }
